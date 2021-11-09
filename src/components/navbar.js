10207135e19a408bacba0277e2e234d26f021b98
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2021-11-09 15:50:12 -0500

    navbar link build out w/css styling

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,24 +1,28 @@
 import React from 'react'
 
+// NAVBAR //
 const navbar = () => {
     return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <a className="navbar-brand" href="#">Navbar</a>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-    </button>
-
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-        <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only"></span></a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
-        </li>
-        </ul>
-    </div>
-    </nav>
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+            <div className="container">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mx-auto">
+                        <li className="nav-item">
+                            <a className="nav-link">Home</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link">About</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link">Work</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     )
 }
 
