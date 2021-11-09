14a61704a42418fb1fd187b8fae1cb7cd391a414
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2021-11-09 17:48:52 -0500

    troubleshooting particles.js, will return to fix after more site build out

--- src/components/header.js
+++ src/components/header.js
@@ -6,10 +6,11 @@ import Typed from 'react-typed';
 const header = () => {
     return (
         <div className="header-wrapper">
+            {/* <div id="particles-js"></div> */}
             <div className="main-info">
                 <div className="header-text">
                     <h1>I'm Hannah McDonald.</h1>
-                    {/* React Typed npm package */}
+                    {/* React-Typed npm package */}
                     <Typed 
                     className="typed-text"
                     strings={["I'm an Atlanta based Full-Stack Web Developer."]}
@@ -17,13 +18,15 @@ const header = () => {
                     />
                 </div>
             </div>
+                {/* Particles npm package */}
+                {/* <div className="particle">
+                    <Particles
+                    params={particles}
+                    className="particles"
+                    />
+                </div>  */}
+            {/* <div id="particles-js"></div> */}
          </div>
-         /* <div className="particle">
-            <Particles
-            params={particles}
-            className="particles"
-            />
-         </div> */
     )
 }
 
