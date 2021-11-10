683a5f2169611fd76737b0f30ec9db1d48f43e11
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2021-11-09 20:54:30 -0500

    footer created with working links && react-icon svg icons

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,34 @@
 // Import React //
 import React from 'react';
+// Social Link Icons from React-Icons //
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { FaEnvelope } from 'react-icons/fa';
+
 
 const footer = () => {
     return (
-        <div>
-
-        </div>
+        <footer>
+            <div className= "container py-5 footer-container">
+                <div className="row">
+                    <div className="twelve columns">
+                        <div className="social-links">
+                            <a href="https://www.linkedin.com/in/hannah-mcdonald-302292215/">
+                                <FaLinkedin className="icons"/>
+                            </a>
+                            <a href= "mailto: hannahcodes@protonmail.com">
+                                <FaEnvelope className="icons"/>
+                            </a>
+                            <a href= "https://github.com/hannahnmcdonald">
+                                <FaGithub className="icons"/>
+                            </a>
+                        </div>
+                            <p> Created by Hannah McDonald ♥️</p>
+                        </div>
+                    <div id="go-top"><a className="smoothscroll" title="Back to Top" href="#home"><i className="icon-up-open"></i></a></div>
+                </div>
+            </div>
+        </footer>
     )
 }
 
