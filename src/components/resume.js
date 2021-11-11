89215228d752e7e5f455d508a7f0e47a04c10e15
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2021-11-11 14:48:46 -0500

    formatting resume section

--- src/components/resume.js
+++ src/components/resume.js
@@ -8,12 +8,13 @@ const resume = () => {
         <div className="container py-5">
             <div className="row">
                 <h1>Resume</h1>
+                <h6>You can download a PDF of my resume here</h6>
                 <div className="columns download">
                     <p>
                         {/* TO DO: EMBED RESUME LINK */}
-                        <a href= "/" className="button download-button">
+                        <button className="resume" href= "https://docs.google.com/document/d/1CHkaG-6mMA2N7rDirFmvaa9xIG7X2dT8qJSUxdgFj6M/edit?usp=sharing" className="button download-button" download>
                             <FaFileDownload className= "resume"/> Download Resume
-                        </a>
+                        </button>
                     </p>
                 </div>
             </div>
