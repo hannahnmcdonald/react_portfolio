59107dc5c0b9945d8e47930a62861a6ac38f3224
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2021-11-11 10:56:35 -0500

    basic works section complete- still would like to add technologies and brief summary to each project

--- src/components/work.js
+++ src/components/work.js
@@ -3,73 +3,116 @@ import React from 'react';
 // Portfolio Images //
 import book from "../img/book.png"
 import heyo from "../img/heyo.png"
-import trail from "../img/trail.png"
+import blog from "../img/blog.png"
 import fitness from "../img/fitness.png"
 import expanse from "../img/expanse.png"
 import weather from "../img/weather.png"
 // React-Icons //
 import { GoChevronRight } from 'react-icons/go';
 import { VscCode } from 'react-icons/vsc';
+// // Pop Up Box //
+// import { PopupManager, Popupboxcontainer } from 'react-popupbox';
 
 const work = () => {
     return (
-        <div className="portfolio-wrapper"> 
-            <div className="container">
-                <h1 className="text-uppercase text-center py-5">
-                    Work
-                </h1>
-                <div className= "image-box-wrapper row justify-content-center">
-                    <div className="portfolio-image-box">
-                        <img className="portfolio-image" src={ book } alt="Book Club Collective..." />
-                        <div className="overflow"></div>
-                            <GoChevronRight className="icons portfolio-icon" />
-                            <VscCode className="icons portfolio-icon" />
-                        <div class="portfolio-links">
-                            <a href="https://github.com/hannahnmcdonald/Book-Club-Collective" title="More Details"><i class="bx bx-link"></i></a>
-                            <a href="https://intense-tor-64466.herokuapp.com/" title="Website"><i class="bx bx-plus"></i></a>
-                        </div>
-                    </div>
-                    <div className="portfolio-image-box">
-                        <img className="portfolio-image" src={ heyo } alt="Heyo Simple Strategic Moves..." />
-                        <div className="overflow"></div>
-                            <GoChevronRight className="icons portfolio-icon" />
-                            <VscCode className="icons portfolio-icon" />
-                        <div class="portfolio-links portfolio-icon">
-                            <a href="https://github.com/OSKWalker/Heyo-Simple-Strategic-Moves" title="More Details"><i class="bx bx-link"></i></a>
-                            <a href="https://oskwalker.github.io/Heyo-Simple-Strategic-Moves/" title="Website"><i class="bx bx-plus"></i></a>
-                        </div>
-                    </div>
-                    <div className="portfolio-image-box">
-                        <img className="portfolio-image" src={ expanse } alt="The Expanse Trivia Quiz..." />
-                        <div className="overflow"></div>
-                            <GoChevronRight className="icons portfolio-icon" />
-                            <VscCode className="icons portfolio-icon" />
-                    </div>
-                    <div className="portfolio-image-box">
-                        <img className="portfolio-image" src={ weather } alt="Weather Dashboard..." />
-                        <div className="overflow"></div>
-                            <GoChevronRight className="icons portfolio-icon" />
-                            <VscCode className="icons portfolio-icon" />
-                        <div class="portfolio-links">
-                        <a href="https://github.com/hannahnmcdonald/WeatherDashboard" title="More Details"><i class="bx bx-link"></i></a>
-                        <a href="https://hannahnmcdonald.github.io/WeatherDashboard/" title="Website"><i class="bx bx-plus"></i></a>
-                    </div>
-                    </div>
-                    <div className="portfolio-image-box">
-                        <img className="portfolio-image" src={ fitness } alt="Fitness Tracker..." />
-                        <div className="overflow"></div>
-                            <GoChevronRight className="icons portfolio-icon" />
-                            <VscCode className="icons portfolio-icon" />
-                    </div>
-                    <div className="portfolio-image-box">
-                        <img className="portfolio-image" src={ trail } alt="Trail Finder..." />
-                        <div className="overflow"></div>
-                            <GoChevronRight className="icons portfolio-icon" />
-                            <VscCode className="icons portfolio-icon" />
-                    </div>
+    <section id="portfolio" className="portfolio section-bg">
+        <div className="container">
+  
+          <div className="section-title">
+            <h1>Work</h1>
+          </div>
+
+  
+          <div className="row portfolio-container">
+  
+            <div className="col-lg-4 col-md-6 portfolio-item filter-app">
+              <div className="portfolio-wrap">
+                <img src={ book } className="img-fluid" alt="day planner"/>
+                <div className="portfolio-info">
+                  <h4>Book Club Collective</h4>
+                  <p></p>
                 </div>
+                <div class="portfolio-links">
+                    <a href="https://github.com/hannahnmcdonald/Book-Club-Collective" title="More Details"><VscCode className="icons portfolio-icon" /></a>
+                    <a href="https://intense-tor-64466.herokuapp.com/" title="Website"><GoChevronRight className="icons portfolio-icon" /></a>
+                </div>
+              </div>
             </div>
+  
+            <div className="col-lg-4 col-md-6 portfolio-item filter-card">
+              <div className="portfolio-wrap">
+                <img src={ heyo } className="img-fluid" alt=""/>
+                <div className="portfolio-info">
+                  <h4>Heyo! Simple Strategic Moves</h4>
+                  <p></p>
+                </div>
+                <div className="portfolio-links">
+                    <a href="https://github.com/hannahnmcdonald/Heyo-Simple-Strategic-Moves" title="More Details"><VscCode className="icons portfolio-icon" /></a>
+                    <a href="https://oskwalker.github.io/Heyo-Simple-Strategic-Moves/" title="Website"><GoChevronRight className="icons portfolio-icon" /></a>
+                </div>
+              </div>
+            </div>
+            
+            <div className="col-lg-4 col-md-6 portfolio-item filter-app">
+              <div className="portfolio-wrap">
+                <img src={ weather } className="img-fluid" alt="weather dashboard app"/>
+                <div className="portfolio-info">
+                  <h4>Weather Dashboard</h4>
+                  <p></p>
+                </div>
+                <div className="portfolio-links">
+                    <a href="https://github.com/hannahnmcdonald/WeatherDashboard" title="More Details"><VscCode className="icons portfolio-icon" /></a>
+                    <a href="https://hannahnmcdonald.github.io/WeatherDashboard/" title="Website"><GoChevronRight className="icons portfolio-icon" /></a>
+                </div>
+              </div>
+            </div>
+  
+            <div className="col-lg-4 col-md-6 portfolio-item filter-web">
+              <div className="portfolio-wrap">
+                <img src={ blog } className="img-fluid" alt=""/>
+                <div className="portfolio-info">
+                  <h4>Tech Talk- A Tech Blog </h4>
+                  <p></p>
+                </div>
+                <div className="portfolio-links">
+                    <a href="https://github.com/hannahnmcdonald/Tech-Talk" title="More Details"><VscCode className="icons portfolio-icon" /></a>
+                    <a href="https://tech-talk-blog.herokuapp.com/" title="Website"><GoChevronRight className="icons portfolio-icon" /></a>
+                </div>
+              </div>
+            </div>
+  
+            <div className="col-lg-4 col-md-6 portfolio-item filter-web">
+              <div className="portfolio-wrap">
+                <img src= { fitness } className="img-fluid" alt=""/>
+                <div className="portfolio-info">
+                  <h4>Fitness Tracker</h4>
+                  <p></p>
+                </div>
+                <div className="portfolio-links">
+                    <a href="https://github.com/hannahnmcdonald/FitnessFocus" title="More Details"><VscCode className="icons portfolio-icon" /></a>
+                    <a href="https://fitness-focus.herokuapp.com/" title="Website"><GoChevronRight className="icons portfolio-icon" /></a>
+                </div>
+              </div>
+            </div>
+  
+            <div className="col-lg-4 col-md-6 portfolio-item filter-web">
+              <div className="portfolio-wrap">
+                <img src= { expanse } className="img-fluid" alt=""/>
+                <div className="portfolio-info">
+                  <h4>The Expanse Trivia Quiz</h4>
+                  <p></p>
+                </div>
+                <div className="portfolio-links">
+                    <a href="https://hannahnmcdonald.github.io/TheExpanseTriviaQuiz/" title="More Details"><VscCode className="icons portfolio-icon" /></a>
+                    <a href="https://github.com/hannahnmcdonald/TheExpanseTriviaQuiz" title="Website"><GoChevronRight className="icons portfolio-icon" /></a>
+                </div>
+              </div>
+            </div>
+  
+          </div>
+  
         </div>
+    </section>
     )
 }
 
