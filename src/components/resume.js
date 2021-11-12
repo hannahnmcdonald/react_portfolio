ec63b20a3244ef4388546a4978dc2078b007670b
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2021-11-11 20:53:38 -0500

    last update to particles

--- src/components/resume.js
+++ src/components/resume.js
@@ -2,8 +2,6 @@
 import React from 'react';
 // Download Icon from React-Icons //
 import { FaFileDownload } from 'react-icons/fa';
-// Import Resume PDF //
-// import { resumepdf } from '../pdf/hannahmcdonaldresume2021.pdf';
 
 const resume = () => {
     return (
@@ -11,8 +9,6 @@ const resume = () => {
             <div className="row">
                 <h1>Resume</h1>
                 <h6>You can download a PDF of my resume here</h6>
-                {/* <h6></h6>
-                <h6></h6> */}
                 <div className="columns download">
                     {/* TO DO: EMBED RESUME LINK */}
                     <a href= "https://drive.google.com/file/d/1EBrP-OA3QyAsXZ4pOpjnlX6na7lpyrJa/view?usp=sharing" className="button download-button">
