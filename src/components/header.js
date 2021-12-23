e7839c97a4d4c4b5cef76c46fc362e3eef56b392
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2021-12-23 13:33:23 -0500

    fixed depreciated particles npm package- replaced with react-tsparticles

--- src/components/header.js
+++ src/components/header.js
@@ -3,30 +3,31 @@ import React from 'react';
 // Import React-Typed //
 import Typed from 'react-typed';
 // Import Particles //
-import Particles from 'react-particles-js';
+import Particles from "react-tsparticles";
 import particles from '../const/particles';
 
 const header = () => {
     return (
         <div className="header-wrapper">
-            {/* React Particles.js npm package */}
-            <div>
-                <Particles
-                    params={particles}
-                    className="particles"
-                />
-            </div> 
+            {/* <Particles
+                params={particles}
+                className="particles"
+            /> */}
             <div className="row banner">
                 <div className="main-info banner-text" id="overlay">
                     <div className="header-text responsive-headline">
                         <h1 className="responsive-headline"> I'm Hannah McDonald.</h1>
-                        {/* React-Typed npm package */}
+
                         <Typed 
                         className="typed-text"
                         strings={["I'm an Atlanta based <b> Full-Stack Web Developer.</b>"]}
                         typeSpeed={50}
                         />
                     </div>
+                    <Particles
+                        params={particles}
+                        className="particles"
+                    />
                 </div>
             </div>
          </div>
