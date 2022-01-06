c5c94d4c8d6d5527e3662951065d51eddb99bf1b
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2022-01-06 18:01:15 -0500

    restructure about

--- src/components/about.js
+++ src/components/about.js
@@ -17,7 +17,7 @@ const about = () => {
                        About
                     </h1>
                     <h6 className="about-text">
-                    I'm a beginner Full-Stack Web Developer. In my spare time I like running, hiking, photography, and traveling. I started out teaching myself programming as a hobby, and I loved it immediately. I then enrolled in a Bootcamp for a more structured education so I could pursue a career in it in the future. I have learned so much from my bootcamp instructors as well as from freecodecamp, Udemy, r/LearnProgramming, and Stack Overflow. I love learning and believe that learning is a lifelong journey to improve oneself. Check out my work below!
+                    I'm a Full-Stack Web Developer. In my spare time I like running, hiking, photography, and traveling. I started out teaching myself programming as a hobby, and I loved it immediately. I then enrolled in a Bootcamp for a more structured education so I could pursue a career in it in the future. I have learned so much from my bootcamp instructors as well as from freecodecamp, Udemy, r/LearnProgramming, and Stack Overflow. I love learning and believe that learning is a lifelong journey to improve oneself. Check out my work below!
                     </h6>
                </div>
             </div>
