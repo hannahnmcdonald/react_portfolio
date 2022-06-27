a3565b4e0fe0ca8ec10670d2402f06645f51d9f2
hannahnmcdonald <hannahnmcdonald@protonmail.com>
2022-06-27 19:37:20 -0400

    refractored portfolio/work page & updated resume link

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -2,8 +2,8 @@
 import React from 'react';
 // Portfolio Images //
 import book from "../img/book.png"
-import heyo from "../img/heyo.png"
-import blog from "../img/blog.png"
+// import heyo from "../img/heyo.png"
+// import blog from "../img/blog.png"
 import checkmate from "../img/profile.png"
 import expanse from "../img/expanse.png"
 import weather from "../img/weather.png"
@@ -15,59 +15,67 @@ import { VscCode } from 'react-icons/vsc';
 
 const work = () => {
     return (
-    <section id="portfolio" className="portfolio section-bg">
+    <section id="portfolio" className="portfolio row">
         <div className="container">
   
-          <div className="section-title">
+          <div className="section-title pb-4">
             <h1>Work</h1>
           </div>
 
+          <div className="row">
   
-          <div className="row portfolio-container">
-  
-            <div className="col-lg-4 col-md-6 portfolio-item filter-app">
-              <div className="portfolio-wrap">
-                <img src={ book } className="img-fluid" alt="Book Club COllective"/>
-                <div className="portfolio-info">
-                  <h4>Book Club Collective</h4>
-                  <p className="desc">A full stack app where users can search for books using Google’s book API, leave reviews, and join book clubs with other readers.<b>Technologies: Handlebars, Node.js, MySQL, Sequelize, Express, and bcrypt.</b></p>
+            <div className="col-lg-12 col-md-10 p-4">
+              <div className="row text-center align-items-center">
+                <div className="col-sm">
+                  <img src={ book } className="img-fluid" alt="Book Club COllective"/>
                 </div>
-                <div class="portfolio-links">
+                <div className="col-sm desc">
+                  <h4>Book Club Collective</h4>
+                  <p className="desc">A full stack app where users can search for books using Google’s book API, leave reviews, and join book clubs with other readers. Book Club Collective uses a folder structure that matches the MVC paradigm as well as Node.js and Express to create a RESTful API. The site also uses MySQL and Sequelize for the database and npm star-rating.js to create a star rating system out of 5 for the book reviews and then averages them out per book.</p>
+                  <p><b>Technologies: Handlebars, Node.js, MySQL, Sequelize, Express, and bcrypt.</b></p>
+                  <div class="portfolio-links">
                     <a href="https://github.com/hannahnmcdonald/Book-Club-Collective" title="Github Repo"><VscCode className="icons portfolio-icon" /></a>
                     <a href="https://intense-tor-64466.herokuapp.com/" title="Website"><GoChevronRight className="icons portfolio-icon" /></a>
                 </div>
+                </div>
               </div>
             </div>
 
-            <div className="col-lg-4 col-md-6 portfolio-item filter-web">
-              <div className="portfolio-wrap">
-                <img src= { checkmate } className="img-fluid" alt="Check-Mate"/>
-                <div className="portfolio-info">
+            <div className="col-lg-12 col-md-10 p-4">
+              <div className="row text-center align-items-center">
+                <div className="col-sm desc">
                   <h4>Check Mate</h4>
-                  <p>CheckMate is the app to keep track of your game nights with friends and family! <b>Technologies: Js, MongoDB, Apollo, GraphQL, React, MaterialUI</b></p>
-                </div>
-                <div className="portfolio-links">
+                  <p className="desc">CheckMate is an app to keep track of your game nights with friends and family! Check Mate is a full stack application enabling a user to create a profile, add friends, and create friend groups. Users can begin game events and record the results to the participating players' individual stats, stored in a Mongo Database, and shown on their profile, along with some of their favorite games. Users are able to research the stats of their friends and themselves. They can also submit and review alternate game rule sets for specific games also stored in a Mongo database and also are able to access game specific rules.</p>
+                  <p><b>Technologies: Js, MongoDB, Apollo, GraphQL, React, MaterialUI</b></p>
+                  <div className="portfolio-links">
                     <a href="https://github.com/hannahnmcdonald/check-mate" title="Github Repo"><VscCode className="icons portfolio-icon" /></a>
                     <a href="https://check-mate-game-app.herokuapp.com/" title="Website"><GoChevronRight className="icons portfolio-icon" /></a>
+                  </div>
+                </div>
+                <div className="col-sm">
+                  <img src= { checkmate } className="img-fluid" alt="Check-Mate"/>
                 </div>
               </div>
             </div>
 
-            <div className="col-lg-4 col-md-6 portfolio-item filter-web">
-              <div className="portfolio-wrap">
-                <img src= { expanse } className="img-fluid" alt="Quiz"/>
-                <div className="portfolio-info">
+            <div className="col-lg-12 col-md-10 p-4">
+              <div className="row text-center align-items-center">
+                <div className="col-sm">
+                  <img src= { expanse } className="img-fluid" alt="Quiz"/>
+                </div>
+                <div className="col-sm desc ">
                   <h4>The Expanse Trivia Quiz</h4>
                   <p>Play this timed quiz game for the hit sci-fi series 'The Expanse.' This quiz follows questions from seasons 1-5. Test your knowledge, save your high score, and play against your friends!</p>
-                </div>
-                <div className="portfolio-links">
+                  <p><b>Technologies: Vanilla Javascript, HTML, CSS</b></p>
+                  <div className="portfolio-links">
                     <a href="https://github.com/hannahnmcdonald/TheExpanseTriviaQuiz/" title="Github Repo"><VscCode className="icons portfolio-icon" /></a>
                     <a href="https://hannahnmcdonald.github.io/TheExpanseTriviaQuiz/" title="Website"><GoChevronRight className="icons portfolio-icon" /></a>
+                  </div>
                 </div>
               </div>
             </div>
   
-            <div className="col-lg-4 col-md-6 portfolio-item filter-card">
+            {/* <div className="col-lg-4 col-md-6 portfolio-item filter-card">
               <div className="portfolio-wrap">
                 <img src={ heyo } className="img-fluid" alt="Heyo"/>
                 <div className="portfolio-info">
@@ -107,9 +115,8 @@ const work = () => {
                     <a href="https://tech-talk-blog.herokuapp.com/" title="Website"><GoChevronRight className="icons portfolio-icon" /></a>
                 </div>
               </div>
-            </div>
+            </div> */}
 
-  
           </div>
   
         </div>
